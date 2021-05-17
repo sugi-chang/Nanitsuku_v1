800ad08ba612ddb513cfc311463fae217883b59c
Yuuri <yuurisugiyama@gmail.com>
2021-05-18 08:53:26 +0900

    Initial Commit

---
+++ Nanitsuku_v1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nanitsuku_v1
+//
+//  Created by 杉山湧里 on 2021/05/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nanitsuku_v1/Nanitsuku_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Nanitsuku_v1App.swift
+//  Nanitsuku_v1
+//
+//  Created by 杉山湧里 on 2021/05/18.
+//
+
+import SwiftUI
+
+@main
+struct Nanitsuku_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
